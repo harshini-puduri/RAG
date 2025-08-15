ad2da8abe288173f40df4f52275d8201330d57a1
harshini-puduri <sriharshinipuduri@gmail.com>
2025-08-14 21:35:45 -0400

    rrf

--- 1 - Advanced RAG/RRF.py
+++ 1 - Advanced RAG/RRF.py
@@ -9,11 +9,17 @@ def reciprocal_rank_fusion(ranked_lists:list[list], k:int)->list:
                 rrf_scores[doc_id] += score
             else: rrf_scores[doc_id] = score
         
-    sorted_docs = sorted(rrf_scores.items(), key=lambda item: item[1], reverse=True)
+    sorted_docs = sorted(rrf_scores.items(), key=lambda item: item[1], reverse=True) 
+    """
+    rrf_scores.items() -> turns the dictionary into an iterable of (key,value) tupes
+    rrf_scores = {'doc_1':2.5, 'doc_2': 4.0, 'doc_3': 1.7} -> [('doc_1',2.5), ('doc_2',4.0), ('doc_3',1.7)]
+    key = lambda item: item[1] -> tells sorted to look at the second element of each tuple when sorting
+    reverse = True -> sorts in DESC order; reverse = false is default
+    """
 
     return [doc_id for doc_id, score in sorted_docs]
 
-keyword_results = #results from retriever BM25
-semantic_results = #results from rrf
+keyword_results = ['docA', 'docB', 'docC'] #results from retriever BM25
+semantic_results = ['doc_A', 'doc_B', 'doc_C']#results from retriever SentenceTransformer
 
 ensemble_results = reciprocal_rank_fusion([keyword_results,semantic_results])
